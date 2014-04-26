37a464b826aa4af6c88c6681937833ece9a0293e
Mark Reale <mark@reale.ca>
2014-04-26 14:59:04 -0400

    initial commit of theme files for project foundation

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+        <div class="footer--bg">
+            <footer>
+                <span>&copy; Project Lightning</span>
+            </footer>
+        </div> <!-- .footer--bg -->
+
+    </body>
+</html>
