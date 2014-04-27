3128abd84a420f650f1a4f3a94af5903c570f87e
Mark Reale <mark@reale.ca>
2014-04-26 20:23:09 -0400

    implement home screen mobile design

--- t_home.php
+++ t_home.php
@@ -1,8 +1,12 @@
 <?php /* Template Name: Home */
 get_header(); ?>
-<nav>
-    <a class="btn" href="mailto:solomon@vividsensesdesign.com">email</a>
-    <a class="btn" href="tel:416-924-2784">phone</a>
+
+<nav class="utilities">
+    <a class="home_link" href="<?php bloginfo('url'); ?>" title="Vivid Senses Design">
+        <img src="<?php bloginfo('template_directory'); ?>/images/vivid_senses_logo.png" title="Vivid Senses Logo" alt="Vivid Senses Logo" />
+    </a>
+    <a class="btn" href="mailto:solomon@vividsensesdesign.com" target="_blank">email</a>
+    <a class="btn" href="tel:647-282-7324" target="_blank">phone</a>
 </nav>
 <header>
     <span class="help">
@@ -14,5 +18,21 @@ get_header(); ?>
         <span class="web">Web</span>
     </h1>
 </header>
+
+<nav class="details">
+    <a class="btn_about" href="#" title="About">
+        Find out how we have you covered
+    </a>
+
+    <a class="btn_philosophy" href="#" title="Philosophy">
+        Our Philosophy
+    </a>
+
+    <a class="btn_work" href="#" title="">
+        Our Work
+    </a>
+</nav>
+
+
 
 <?php get_footer(); ?>
