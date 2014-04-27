4b70d8eccba665664daae17f84f1da072919ea24
Solomon Hsu <t.z.solomonhsu@gmail.com>
2014-04-26 20:38:39 -0400

    Initial files for Sushi restaurants

--- functions.php
+++ functions.php
@@ -2,4 +2,24 @@
     register_sidebar();
 
     add_theme_support('post-thumbnails');
+
+	add_action('init','create_menu_content_type');
+
+	function create_menu_content_type(){
+		$labels = array(
+		 'name' => ('Menu')
+		 );
+
+		$args = array(
+			'labels' => $labels,
+			'public' => true,
+			'taxonomies' => array('category'),
+			'has_archive' => true,
+			'supports' => array('title','editor','excerpt','thumbnail','page-attributes'),
+			'has_archive' => true
+		);
+
+		register_post_type('Menu',$args);
+	}
+
 ?>
